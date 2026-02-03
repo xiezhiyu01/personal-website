243a8db3a5a64e84715f6f576b7ae8266d4253b1
xiezhiyu01 <zhiyuxie@stanford.edu>
2026-02-03 01:45:59 +0000

    Deploying to gh-pages from @ xiezhiyu01/personal-website@cdef509c9eabccc4b7258157ab5fc6435315de97 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-awards",
           title: "Awards",
-          description: "Selected Honors and Awards.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/awards/";
